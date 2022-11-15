67deaaa2f572452057f0d535ee669f103d42bedd
jeffreyvavasour <93945866+jeffreyvavasour@users.noreply.github.com>
2022-11-15 16:22:18 +0000

    Add files via upload

--- portfolio/dist/js/main.js
+++ portfolio/dist/js/main.js
@@ -13,3 +13,13 @@ navToggle.addEventListener('click', function() {
         navToggle.style.position = 'absolute';
     }
 });
+
+document.addEventListener('click', (e) => {
+    if (e.target.id !== 'nav' && e.target.id !== 'mobile-nav-toggle' && e.target.id !== 'bar-1' && e.target.id !== 'bar-2' && e.target.id !== 'bar-3' && e.target.id !== 'ul') {
+        nav.classList.remove('change');
+        bars.forEach(bar => {
+            bar.classList.remove('change');
+        })
+        navToggle.style.position = 'absolute';
+    };
+});
